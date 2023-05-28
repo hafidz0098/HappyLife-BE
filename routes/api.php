0dfdb546ca51cac7f428630ccc123c3f6f944501
hafidz0098 <hafidznak123@gmail.com>
2023-05-28 17:42:20 +0700

    add API for logout

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,8 @@ Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('lo
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+/**
+ * route "/logout"
+ * @method "POST"
+ */
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
